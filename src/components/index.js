46b431c5558a45343bc87a955ab1f2cb33008e91
ITRO-anujkumar <Anujkumarg816@gmail.com>
2025-07-28 18:10:21 +0530

    Delete src/components/index.js

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import Navbar from "./Navbar.jsx";
-import GlowyBackground from "./GlowyBackground.jsx";
-import HeroSection from "./HeroSection.jsx";
-
-export {
-    Navbar,
-    GlowyBackground,
-    HeroSection
-}
